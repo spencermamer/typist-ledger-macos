e50fb12baa91a604e0445a119fff9c80172d1c93
Spencer B. Mamer <spencermamer@gmail.com>
2023-01-14 12:11:30 -0700

    Initial Commit

---
+++ Typist Ledger/Typist_LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Typist_LedgerApp.swift
+//  Typist Ledger
+//
+//  Created by Spencer Mamer on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Typist_LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
